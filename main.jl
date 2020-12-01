00c8bedd1a07a02772cb4a77f5763520c540bfd2
Lincoln Quirk <lincolnq@gmail.com>
2020-12-01 18:16:04 -0500

    improve the code slightly using `prod`

--- main.jl
+++ main.jl
@@ -3,6 +3,6 @@ parsed = parse.(Int, lines)
 println("args=$parsed")
 
 
-sums = [(x, y, z, x + y + z) for x in parsed for y in parsed for z in parsed if x + y + z == 2020]
+sums = [(x, y, z) for x in parsed for y in parsed for z in parsed if x + y + z == 2020]
 f = first(sums)
-println("sums=$sums, product=$(f[1] * f[2] * f[3])")
+println("sums=$sums, product=$(prod(f))")
