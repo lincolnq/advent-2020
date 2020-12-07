31be425c3efddbf01accc8e716b44739bba16c79
Lincoln Quirk <lincolnq@gmail.com>
2020-12-07 10:51:07 -0500

    solve 7a

--- main.jl
+++ main.jl
@@ -1,8 +1,12 @@
-include("p6.jl")
+include("p7.jl")
 
-groups = split(read(first(ARGS), String), "\n\n")
-questions = parseGroupQuestions.(groups)
+rules = Dict(parseRule.(readlines(first(ARGS))))
 
-sumcounts = sum(length.(questions))
+@memoize function transitivelyHolds(rule, bag)
+    println("transitively holds($rule, $bag)")
+    directlyHolds(rule, bag) || any(transitivelyHolds(rules[b[2]], bag) for b in rule)
+end
 
-println("questions=$questions, sumcounts=$sumcounts")
+const goal = "shiny gold"
+results = [(r, transitivelyHolds(rules[r], goal)) for r in keys(rules)]
+println("number of bag types that can hold $goal = $(sum(x[2] for x in results))")
---
+++ p7.jl
@@ -0,0 +1,20 @@
+using Memoize
+
+__revise_mode__ = :evalassign
+const rule = r"(\w+ \w+) bags? contain (.*)\."
+const containmentOption = r"\s*(\d+) (\w+ \w+) bags?"
+function parseRule(r)
+    m = match(rule, r)
+    outer = m[1]
+    inners = [match(containmentOption, x) for x in split(m[2], ",")]
+    if isnothing(first(inners))
+        innerRules = []
+    else
+        innerRules = [(parse(Int64, x[1]), convert(String, x[2])) for x in inners]
+    end
+    (convert(String, outer), innerRules)
+end
+
+
+
+@memoize directlyHolds(rule, bag) = any(x[2] == bag for x in rule)
